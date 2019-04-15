7ec98e6d1b6f16e7aac00990b57a735db15704ba
Appke <appke@AppkedeMacBook-Air.local>
2019-04-15 20:36:17 +0800

    jar bug

--- src/main/java/njuics/demos/petsalon/MainController.java
+++ src/main/java/njuics/demos/petsalon/MainController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import njuics.demos.petsalon.User;
-import njuics.demos.petsalon.UserRepository;
-
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
--- src/main/java/njuics/demos/petsalon/UserRepository.java
+++ src/main/java/njuics/demos/petsalon/UserRepository.java
@@ -2,7 +2,6 @@ package njuics.demos.petsalon;
 
 
 import org.springframework.data.repository.CrudRepository;
-import njuics.demos.petsalon.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
